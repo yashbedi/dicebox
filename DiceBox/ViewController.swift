5302313c15e963c83ab3c448a359ca4fdc3e7057
Yash Bedi <yash@yashs-macbook-air.local>
2018-02-26 22:53:13 +0530

    Initial Commit

---
+++ DiceBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceBox
+//
+//  Created by Yash Bedi on 26/02/18.
+//  Copyright © 2018 Yash Bedi LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
